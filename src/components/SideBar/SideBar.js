5281452bdacebc5915b0e6f55d0159c3ab5c8b6e
Yrii_T <tarasevichyrii@gmail.com>
2017-12-08 12:17:59 +0300

    side bar

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,25 +1,14 @@
 import React from 'react'
+import Icons from './components'
 import './SideBar.scss'
 
 const SideBar = () => (
   <div className='side_bar'>
-    <div className='row_side'>
-      <div className="title">
-        Wizard is running
-      </div>
-    </div>
-    <div className='row_side'>
-      <div className="title">Product code input</div>
-    </div>
-    <div className='row_side'>
-      <div className="title">Search by name</div>
-    </div>
-    <div className='row_side'>
-      <div className="title">Call for assistance</div>
-    </div>
-    <div className='row_side'>
-      <div className="title">Promo catalogue</div>
-    </div>
+    <Icons icon='Wizard' label='Wizard is running' />
+    <Icons icon='Product' label='Product code input' />
+    <Icons icon='Search' label='Search by name' />
+    <Icons icon='Call' label='Call for assistance' />
+    <Icons icon='Promo' label='Promo catalogue' />
   </div>
 )
 export default SideBar
