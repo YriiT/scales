aeaa8b296cfd13b30ffb260e09d43a88ca65cb26
Yrii_T <tarasevichyrii@gmail.com>
2017-12-09 14:19:11 +0300

    add categories

--- src/components/PicturesView/PicturesView.js
+++ src/components/PicturesView/PicturesView.js
@@ -11,55 +11,54 @@ class PicturesView extends React.Component {
   render() {
     return (
       <div className='content'>
-        <div className="col" data-cat="berries">
-          <a href="category.html">
-            <div className="image">
-              <img src="./img/berries.jpg" />
-            </div>
-            <div className="view_title">Berries</div>
-          </a>
+        <div className="category_col" data-cat="berries">
+
+          <div className="image">
+            <img src="img/berries.jpg" />
+          </div>
+          <div className="view_title">Berries</div>
+
         </div>
-        <div className="col" data-cat="exotic">
-          <a href="category.html">
-            <div className="image">
-              <img src="./img/exotic.jpg" />
-            </div>
-            <div className="view_title">Exotic</div>
-          </a>
+        <div className="category_col" data-cat="exotic">
+
+          <div className="image">
+            <img src="img/exotic.jpg" />
+          </div>
+          <div className="view_title">Exotic</div>
+
         </div>
-        <div className="col" data-cat="fruits">
-          <a href="category.html">
-            <div className="image">
-              <img src="./img/fuits.png" />
-            </div>
-            <div className="view_title">Fruits</div>
-          </a>
+        <div className="category_col" data-cat="fruits">
+
+          <div className="image">
+            <img src="img/fuits.png" />
+          </div>
+          <div className="view_title">Fruits</div>
+
         </div>
-        <div className="col" data-cat="nuts">
-          <a href="category.html">
-            <div className="image">
-              <img src="./img/nuts.jpg" />
-            </div>
-            <div className="view_title">Nuts</div>
-          </a>
+        <div className="category_col" data-cat="nuts">
+
+          <div className="image">
+            <img src="img/nuts.jpg" />
+          </div>
+          <div className="view_title">Nuts</div>
+
         </div>
-        <div className="col" data-cat="salads">
-          <a href="category.html">
-            <div className="image">
-              <img src="./img/salads.png" />
+        <div className="category_col" data-cat="salads">
+
+          <div className="image">
+            <img src="img/salads.png" />
+          </div>
+          <div className="view_title">
+            Salads
             </div>
-            <div className="view_title">
-              Salads
-            </div>
-          </a>
+
         </div>
-        <div className="col" data-cat="vegetables">
-          <a href="category.html">
-            <div className="image">
-              <img src="./img/vegetables.jpg" />
-            </div>
-            <div className="view_title">Vegetables</div>
-          </a>
+        <div className="category_col" data-cat="vegetables">
+          <div className="image">
+            <img src="img/vegetables.jpg" />
+          </div>
+          <div className="view_title">Vegetables</div>
+
         </div>
       </div>
     )
